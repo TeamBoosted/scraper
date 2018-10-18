6c5ef63c676d041cc941d71ee91362963743f5d1
Daniel Kang <danielkang674@gmail.com>
2018-10-18 17:14:42 -0500

    cron job and dockerfile

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 8082;
 const { scrapeGR } = require('./goodReads');
 const app = express();
 const cron = require('node-cron');
+const { genres } = require('./goodReads/genres.js');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -12,7 +13,7 @@ app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
-cron.schedule('* 18 * * *', () => {
+cron.schedule('20 12 * * *', () => {
   console.log('starting scrape');
-  scrapeGR();
+  scrapeGR(genres);
 });
